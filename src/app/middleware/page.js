471ed8e4c03b82f057e4095ab60444cf0a517b6c
Jeramy Perez <jeramymp@gmail.com>
2025-02-19 22:19:10 -0800

    tt

--- src/app/middleware/page.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabaseClient';
-
-export async function middleware(req) {
-  const { data: { user } } = await supabase.auth.getUser();
-  if (!user) return NextResponse.redirect('/login');
-  return NextResponse.next();
-}
